8b496c78749eb1a7db829c7463526923c7080cb1
vagrant <vagrant@homestead>
2020-01-30 13:33:21 +0000

    Implement delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -34,5 +34,9 @@ class Answer extends Model
         static::created(function ($answer){
            $answer->question->increment('answers_count');
         });
+
+        static::deleted(function ($answer){
+            $answer->question->decrement('answers_count');
+        });
     }
 }
