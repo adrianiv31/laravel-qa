ef451324f15eff3877e3b38926aaf2e6fc51cbf4
vagrant <vagrant@homestead>
2020-02-03 11:41:51 +0000

    Implement many to many relationships and make favorite button work

--- app/Question.php
+++ app/Question.php
@@ -19,10 +19,31 @@ class Question extends Model
         return $this->hasMany(Answer::class);
     }
 
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps();
+    }
+
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+    }
+
+
     public function acceptBestAnswer(Answer $answer)
     {
         $this->best_answer_id = $answer->id;
         $this->save();
+    }
+
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
     }
 
     public function setTitleAttribute($value)
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::get('/questions/{slug}', 'QuestionController@show')->name('questions.show
 //Route::post('/questions/{question}/answers','AnswersController@store')->name('answers.store');
 Route::resource('questions.answers', 'AnswersController')->only(['store', 'edit', 'update', 'destroy']);
 Route::post('/answers/{answer}/accept','AcceptAnswerController')->name('answers.accept');
+
+Route::post('questions/{question}/favorites','FavoritesController@store')->name('questions.favorite');
+Route::delete('questions/{question}/favorites','FavoritesController@destroy')->name('questions.unfavorite');
