eb0fe13bdd4b0d68383bdbaa3495b718c3fd5dd0
vagrant <vagrant@homestead>
2020-02-13 09:06:34 +0000

    Rewrite vote control into vue component

--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,8 +1,6 @@
 <answer :answer="{{ $answer }}" inline-template>
     <div class="media post">
-        @include('shared._vote',[
-                'model' => $answer
-            ])
+        <vote :model="{{ $answer }}" name="answer"></vote>
         <div class="media-body">
             {!! Form::open(['v-if'=>'editing','@submit.prevent'=>'update','url'=>'#']) !!}
             <div class="form-group">
