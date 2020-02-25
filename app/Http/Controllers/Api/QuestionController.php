1a69448a5ca5b24e6326f5f184fe983a197f6573
vagrant <vagrant@homestead>
2020-02-25 16:37:58 +0000

    Create an endpoint to create question

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AskQuestionRequest;
 use App\Http\Resources\QuestionResource;
 use App\Question;
 use Illuminate\Http\Request;
@@ -24,18 +25,23 @@ class QuestionController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $question = $request->user()->questions()->create($request->only('title', 'body'));
+
+        return response()->json([
+            'message' => 'Your question has been submited',
+            'question' => new QuestionResource($question)
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Question  $question
+     * @param \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function show(Question $question)
@@ -46,8 +52,8 @@ class QuestionController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Question  $question
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Question $question)
@@ -58,7 +64,7 @@ class QuestionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Question  $question
+     * @param \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function destroy(Question $question)
