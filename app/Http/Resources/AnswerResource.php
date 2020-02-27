4e5b7ebb41345d52269d2a652b0144f4a1ac5222
vagrant <vagrant@homestead>
2020-02-27 07:33:58 +0000

    Created some endpoints for answer resource

--- app/Http/Resources/AnswerResource.php
+++ app/Http/Resources/AnswerResource.php
@@ -4,27 +4,25 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class QuestionDetailsResource extends JsonResource
+class AnswerResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'title' => $this->title,
             'votes_count' => $this->votes_count,
-            'answers_count' => $this->answers_count,
-            'is_favorited' => $this->is_favorited,
-            'favorites_count' => $this->favorites_count,
+            'is_best' => $this->is_best,
             'body' => $this->body,
             'body_html' => $this->body_html,
-            'created_date' => $this->created_date,
             'user' => $this->user,
+            'question_id' => $this->question_id,
+            'created_date' => $this->created_date,
         ];
     }
 }
